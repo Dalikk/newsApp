0a637bbc14624902e284b923d5dad2fb648da347
Dalikk <dmitriev.alik1999@gmail.com>
2021-11-19 15:02:57 +0900

    connected fonts

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { gStyle } from './styles/style';
+import * as Font from 'expo-font';
+
+const fonts = () => Font.loadAsync({
+  'mt-bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+  'mt-light': require('./assets/fonts/Montserrat-Light.ttf')
+});
 
 export default function App() {
+  const [font, setFont] = useState(false);
+  
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style ={gStyle.main}>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
