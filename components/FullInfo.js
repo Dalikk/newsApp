7fa67066ae5e0f9ee017788abd298feb733e02b8
Dalikk <dmitriev.alik1999@gmail.com>
2021-11-20 00:31:13 +0900

    added to index files FullInfo.js and navigate.js

---
+++ components/FullInfo.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { gStyle } from '../styles/style';
+
+export default function FullInfo({ route }) {
+    // const loadScene = () => {
+    //     navigation.goBack();
+    // }
+    return (
+        <View style ={gStyle.main}>
+            <Text style={gStyle.title}>{route.params.name}</Text>
+            <Text>{route.params.full}</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
